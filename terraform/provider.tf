7703d119949f01983d58439cf2d11a04557ca064
Cipher <91955270+Cipheritall@users.noreply.github.com>
2025-03-04 14:48:32 +0100

    set tf version back 2.9.3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,13 @@
 terraform {
-  required_providers {
-    proxmox = {
-      source  = "telmate/proxmox"
-      version = "~> 3.0.0"
+    required_providers {
+        proxmox = {
+            source = "telmate/proxmox"
+            version = "2.9.3"
+        }
     }
-  }
+
 }
+
 
 provider "proxmox" {
     pm_api_url = var.proxmox_api_url
