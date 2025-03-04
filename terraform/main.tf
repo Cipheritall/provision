74f5514c37564e7ff564212943a8af2e57486301
Cipher <91955270+Cipheritall@users.noreply.github.com>
2025-03-04 14:39:21 +0100

    set vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,30 @@
-resource "proxmox_vm_qemu" "vm" {
+resource "proxmox_vm_qemu" "openHands" {
   name        = "debian-vm"
   target_node = "pve"
-  clone       = "debian-template"
-
-  cpu    = "host"
-  cores  = 2
-  memory = 4096
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
+  vmid        = 200
+  memory      = 4096
+  cores       = 2
+  cpu         = "host"
   disk {
-    storage = "local-lvm"
-    type    = "scsi"
-    size    = "20G"
+    size       = "40G"
+    type       = "virtio"
+    storage    = "local-lvm"
   }
-
-  os_type                  = "cloud-init"
-  cloudinit_cdrom_storage  = "local-lvm"
+  network {
+    model      = "virtio"
+    bridge     = "vmbr0"
+  }
+  boot        = "cdn"
+  bootdisk    = "virtio0"
+  iso         = "local:iso/debian-live-12.9.0-amd64-kde.iso"
   sshkeys                  = <<EOT
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDToWVX12P1WF5vGWdXAWtZ9GPXVoNZMo1jQylMnS3XSStpUwz2HVVPni2klvJP/6+1KJ538TqBLTT4s06IvL+Yjgr0uJIIYx7CAmgEKDl9o3dxIIVywqgg82+ejng3kXl9vSgi7Blk3LJp2rttqNE/xTeomi2ilqppub4t20AiZfk/A21+iqo7FuQov2OgWj9u3x9xVxtkQ9S5AMO51HlsLoGmWeXcKlPViVSB47FMzJl/WUUvyfOTPfMUA3LngZB8XkcI/U+TPb0U9OXcatU1TPGLxWc1vFKb+GngSV2PZ+IoZ4Tn3nv5cQM1Xov+TpB2z7lCrEvD+I8TX9yhe3lPeEOCG/gmO572cTjXeiSIvWdJFX2bYEmTIiVOcu4IbzQAcQR8NmEzYV4oUNYbrY8nmqAOjV8Wa3hnfg/ZhjskL1vp5InmiPEARXrNlkhUvCqkzPI8fNkvJ3fj1qzkx6gxFQEGJzhsW38yPcxHFVYukG2rdKiICsLxPEHrUw76Oa8= run@GitRunners
+  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDToWVX12P1WF5vGWdXAWtZ9GPXVoNZMo1jQylMnS3XSStpUwz2HVVPni2klvJP/6+1KJ538TqBLTT4s06IvL+Yjgr0uJIIYx7CAmgEKDl9o3dxIIVywqgg82+ejng3kXl9vSgi7Blk3LJp2rttqNE/xTeomi2ilqppub4t20AiZfk/A21+iqo7FuQov2OgWj9u3x9xVxtkQ9S5AMO51HlsLoGmWeXcKlPViVSB47FMzJl/WUUvyfOTPfMUA3LngZB8XkcI/U+TPb0U9OXcatU1TPGLxWc1vFKb+GngSV2PZ+IoZ4Tn3nv5cQM1Xov+TpB2z7lCrEvD+I8TX9yhe3lPeEOCG/gmO572cTjXeiSIvWdJFX2bYEmTIiVOcu4IbzQAcQR8NmEzYV4oUNYbrY8nmqAOjV8Wa3hnfg/ZhjskL1vp5InmiPEARXrNlkhUvCqkzPI8fNkvJ3fj1qzkx6gxFQEGJzhsW38yPcxHFVYukG2rdKiICsLxPEHrUw76Oa8= run@GitRunners
 EOT
+}
+
+output "container_credentials" {
+    description = "Default login credentials"
+    value = {
+        root_password = "00221"
+    }
 }
