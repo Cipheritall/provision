66c562ff5426bb7d7fcce9b8c2a405cc7192e823
Cipher <91955270+Cipheritall@users.noreply.github.com>
2025-03-04 14:51:54 +0100

    adapt terraform provider for proxmox

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,10 @@
 terraform {
-    required_providers {
-        proxmox = {
-            source = "telmate/proxmox"
-            version = "2.9.15"
-        }
+  required_providers {
+    proxmox = {
+      source = "TheGameProfi/proxmox"
+      version = "2.9.15"
     }
-
+  }
 }
 
 
