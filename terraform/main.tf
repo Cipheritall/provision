76e4fc76ab73200cbe721e2bcc124ddb87c5f2bb
Cipher <91955270+Cipheritall@users.noreply.github.com>
2025-03-04 14:11:11 +0100

    fix uneeded vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,25 @@
-resource "proxmox_vm_qemu" "openHands" {
+resource "proxmox_vm_qemu" "vm" {
   name        = "debian-vm"
   target_node = "pve"
-  vmid        = 200
-  memory      = 4096
-  cores       = 2
-  cpu         = "host"
-  disk {
-    size       = "40G"
-    type       = "virtio"
-    storage    = "local-lvm"
-  }
+  clone       = "debian-template"
+
+  cpu    = "host"
+  cores  = 2
+  memory = 4096
+
   network {
-    model      = "virtio"
-    bridge     = "vmbr0"
+    model  = "virtio"
+    bridge = "vmbr0"
   }
-  boot        = "cdn"
-  bootdisk    = "virtio0"
-  iso         = "local:iso/debian-live-12.9.0-amd64-kde.iso"
+
+  disk {
+    storage = "local-lvm"
+    type    = "scsi"
+    size    = "20G"
+  }
+
+  os_type       = "cloud-init"
+  ssh_user      = "root"
+  ssh_private_key = file("~/.ssh/id_rsa")
+  cloudinit_cdrom_storage = "local-lvm"
 }
